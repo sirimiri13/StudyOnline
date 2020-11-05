0c3a4f9f7906dec8e1e83baa8c1ad639cf7adb9f
sirimri13 <lamquynhhuong1304@gmail.com>
2020-11-05 19:55:53 +0700

    add AccountPage empty

---
+++ lib/source/AccountPage/AccountPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatefulWidget{
+  static String tag = 'account-page';
+  @override
+  _AccountPage createState() => new _AccountPage();
+}
+
+class _AccountPage extends State<AccountPage>{
+  @override
+  Widget build(BuildContext context){
+  }
+}
